22915ca9dc1d3b0a075cb13cebad7d1a4e5072a3
Abhay <abhaykoli214@gmail.com>
2025-03-11 14:54:46 +0530

    changes

--- Client/src/store/config.js
+++ Client/src/store/config.js
@@ -1,19 +1,17 @@
 const config = {
-  // API_URL: "https://goldfish-app-6bp2q.ondigitalocean.app/api",
+  //:local host
 
-  //:local hoist
-
-  API_URL: "http://localhost:8000/api",
-  CLIENT_ID:
-    "295957989993-3eqvomqkcfhm39ov845blst9v2qboj7n.apps.googleusercontent.com",
+  // API_URL: "http://localhost:8000/api",
+  // CLIENT_ID:
+  //   "295957989993-3eqvomqkcfhm39ov845blst9v2qboj7n.apps.googleusercontent.com",
 
   // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  // live
+  // live host
 
-  // API_URL: "https://rideflowrentals.in/api",
-  // CLIENT_ID:
-  //   "95168833702-gm65l70v96bkac5sr0c9m6jup87o73pr.apps.googleusercontent.com",
+  API_URL: "https://rideflowrentals.in/api",
+  CLIENT_ID:
+    "95168833702-gm65l70v96bkac5sr0c9m6jup87o73pr.apps.googleusercontent.com",
 };
 
 export default config;
