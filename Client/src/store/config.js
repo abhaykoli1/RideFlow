58becd0ad9badd196997de001271c8a7e113e330
abhaykoli1 <abhaykoli214@gmail.com>
2025-05-21 22:43:53 +0530

    changes

--- Client/src/store/config.js
+++ Client/src/store/config.js
@@ -2,7 +2,7 @@ const config = {
   //:local host
 
   // API_URL: "http://localhost:8000/api",
-  API_URL: "https://ride-flow-pi.vercel.app/api",
+  API_URL: "https://ride-flow-iota.vercel.app/api",
   CLIENT_ID:
     "295957989993-3eqvomqkcfhm39ov845blst9v2qboj7n.apps.googleusercontent.com",
 
